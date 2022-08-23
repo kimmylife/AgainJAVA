4629b884f3ac9d3af2b33a2f214da182852945f9
kimmy <dmsql0486@naver.com>
2022-08-23 16:44:49 +0900

    커밋

---
+++ workspace/day02/src/day02/PrintTest2.java
@@ -0,0 +1,24 @@
+package day02;
+
+public class PrintTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		제어문자
+//		반드시 따옴표 안에서 작성
+//		\n : new line, 줄바꿈, 개행문자 
+//		\t : tab, 위아래 줄간격 맞춰 띄우기
+//		\" : " 표현
+//		\' : ' 표현
+//		\\ : \ 표현 
+		
+//		System.out.print("유기현\n");
+//		System.out.print("\"20살\"");
+
+		System.out.println("유기현");
+		System.out.println("20살");
+		
+	}
+
+}
---
+++ workspace/day03/src/day03/Variable.java
@@ -0,0 +1,20 @@
+package day03;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 10;
+		float interestRate = 2.5284F;
+		double score = 2.5284;
+		char grade = 'A';
+		String data = "ABC";
+		
+		System.out.println(age);
+		System.out.println(interestRate);
+		System.out.println(score);
+		System.out.println(grade);
+		System.out.println(data);
+	}
+
+}
